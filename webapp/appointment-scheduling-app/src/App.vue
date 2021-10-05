5eecdd48fbe07ee2b1d8f0a5ce93457eaceac518
Randikaviraj <randikavirajmax@gmail.com>
2021-10-06 01:26:30 +0530

    Vue project created, header component created

---
+++ webapp/appointment-scheduling-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppHeaderVue/>
+</template>
+
+<script>
+import AppHeaderVue from './components/AppHeader.vue'
+
+export default {
+ components:{
+   AppHeaderVue,
+ },
+ 
+}
+</script>
+
+<style>
+</style>
