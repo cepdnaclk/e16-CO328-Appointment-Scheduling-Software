b3da95f7a658fd26b69e077d0fbfa411ad264abc
Tharushi <63853025+TharushiSuwaris@users.noreply.github.com>
2021-10-10 00:19:52 +0530

    Update HomePage.vue

--- webapp/appointment-scheduling-app/src/pages/HomePage.vue
+++ webapp/appointment-scheduling-app/src/pages/HomePage.vue
@@ -9,9 +9,10 @@
       <h2 className="main-heading">Join the world's </h2>
       <p className="main-subheading">Appoiment sheduling journey ...</p>
       <div className="btn-container">
-        <!-- <Link to="/hospitals"> -->
+        <!-- <Link to="/find-service"> -->
+        <router-link to="/find-service">
         <button className="btn btn-outline">FIND SERVICE</button>
-        <!-- </Link> -->
+        </router-link>
         <!-- <Link to="/about"> -->
         <router-link to="/about">
         <button className="btn btn-solid">ABOUT US</button>
